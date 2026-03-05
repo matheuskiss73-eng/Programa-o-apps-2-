3b0b2fd02f14b0cbc2e5ad79f1ffe86818da2486
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 16:44:08 -0300

    feat: .

---
+++ 9-operadores-I.js
@@ -0,0 +1,20 @@
+let idade = 10
+let peso = "10"
+
+console.log("- COMPARAÇÂO -")
+console.log("comparando conteudo")
+console.log(idade == peso)
+
+console.log("\ncomparando tipo e conteudo")
+console.log("Idade e peso são diferentes?")
+console.log(idade === peso)
+
+console.log("- DIFERENÇA -")
+console.log("\ncomparando conteudo")
+console.log("Idade e peso são diferentes?")
+console.log(idade != peso)
+
+console.log("\ncomparando tipo e conteudo")
+console.log("Idade e peso são diferentes?")
+console.log(idade !== peso)
+
