41d0ce5b0e4a82aa639998c48518caf4db6bcbbf
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-03-05 14:44:06 -0300

    feat:ass .

--- 5-if-else-if.js
+++ 5-if-else-if.js
@@ -6,6 +6,6 @@ if (idade >= 18) {
     console.log("maioridade")
 } else if (idade >= 12) {
     console.log("adolescente")
-} else if  (idade <= 2) {
+} else if  (idade < 2) {
     console.log("bebe")
 }
